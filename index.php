936923fc80efad70733b118a2114b56fe12a8523
Khorshed Alom <49843886+nilv0x@users.noreply.github.com>
2020-08-15 20:10:39 +0600

    verify

--- index.php
+++ index.php
@@ -3,9 +3,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap css-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World!</h1>
+    
+    <section class="form-validation">
+        <div class="container">
+            <center class="form-content">
+            <form method="POST" action="verify.php" id="reg-form" class="reg-form">
+                        <h2 class="form-title">Registration Here</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="fname" id="fname" placeholder="Your First Name"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="lname" id="lname" placeholder="Your Last Name"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="email" class="form-input" name="email" id="email" placeholder="Your Email"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="radio" class="form-input" name="gender" id="male" value="male"/>Male
+                            <input type="radio" class="form-input" name="gender" id="female" value="female"/>Female
+                        </div>
+                        <!-- <div class="form-group">
+                            <input type="text" class="form-input" name="fname" id="fname" placeholder="Your First Name"/>
+                        </div> -->
+                        <div class="form-group">
+                            
+                            <input type="text" class="form-input" name="district" id="district" placeholder="District"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-input" name="division" id="division" placeholder="Division"/>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-input" name="pwd" id="pwd" placeholder="Enter Your Password"/>
+                        </div>
+
+                        <input class="btn btn-primary" type="submit" value="Register">
+
+            </form>   
+        </center>   
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ verify.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$fName = $_REQUEST["fname"];
+$lName = $_REQUEST["lname"];
+$mail = $_REQUEST["email"];
+$district = $_REQUEST["district"];
+$division = $_REQUEST["division"];
+
+
+$usrFname = 
+
+echo $fName;
+echo $lName;
+echo $mail;
+echo $district;
+echo $division;
+echo $fName;
+
+
+
+
+?>
